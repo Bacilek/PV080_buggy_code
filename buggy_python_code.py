e23181920f08ee729c7b8f43bfe1b49dd49fcb23
Bacilek <xdoupov@nymfe123.fi.muni.cz>
2026-05-06 13:03:31 +0200

    fixes?

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -39,11 +39,13 @@ def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
+
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
@@ -52,16 +54,19 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
-        new_person = Person("Vickie")  
+
+    if choice == "1":
+        new_person = Person("Vickie")
         print_nametag(input("Please format your nametag: "), new_person)
+
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
         fetch_website(urlib_version, url="https://www.google.com")
+
     elif choice == "3":
         load_yaml(input("File name: "))
         print("Executed -ls on current folder")
+
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
